38992d403a3ef82c21fa62f61a22d0254ade808b
Frank Kotsianas <frankkotsianas@gmail.com>
2018-10-02 18:05:59 -0400

    Move Omniauth config to Devise initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # TODO: need set up CallbacksController as in: https://github.com/leikir/devise-token-auth-omniauth-example/commit/9bc9238899e9dcdf1d1cb9dff448689196102cea
+  # TODO: send CSRF token from frontend eg w axios: https://github.com/leikir/devise-token-auth-omniauth-example/commit/287a13fcb7a4a74411ab8168ce900fb63e0e0376
+  #
 end
