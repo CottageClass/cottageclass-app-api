1460bf5921e98af5d0660e6d204fdee8c94c2cad
Frank Kotsianas <frankkotsianas@gmail.com>
2018-10-25 14:19:26 -0400

    Remove explicit id key from parent.children.attributes
    - since is provided at top level

--- lib/serializers/child_serializer.rb
+++ lib/serializers/child_serializer.rb
@@ -2,8 +2,7 @@ module Serializers
   class ChildSerializer < BaseSerializer
     belongs_to :parent, record_type: :user, serializer: :user_serializer, key: :parent
 
-    attributes :id,
-      :parent_id,
+    attributes :parent_id,
       :first_name,
       :birthday
   end
