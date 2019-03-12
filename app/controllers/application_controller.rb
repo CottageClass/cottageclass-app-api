1be1df52e124b8e86252103cbe44d8c9fe74a531
Suman Debnath <nospam.sd@gmail.com>
2019-03-12 13:36:38 +0530

    devise views, config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # authentication method for ActiveAdmin
   def authenticate_admin!
     master_pass = ENV['ACTIVE_ADMIN_MASTER_PASS']
 
-    auth_success = authenticate_or_request_with_http_basic("Welcome to CottageClass Admin!") do |name, password|
-      name == "admin" && password == master_pass
+    auth_success = authenticate_or_request_with_http_basic('Welcome to CottageClass Admin!') do |name, password|
+      name == 'admin' && password == master_pass
     end
 
-    if (
-        !(master_pass && master_pass.length > 0) ||
-        !auth_success
-    )
-      return false
-    end
+    return false if master_pass.blank? || !auth_success
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: %i[first_name last_name]
+  end
 end
