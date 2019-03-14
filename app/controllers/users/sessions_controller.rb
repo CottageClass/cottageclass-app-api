0aab75b49d2ad1ee9b66c2708b9b7dad1f23117f
Ari Kardasis <ari.kardasis@gmail.com>
2019-03-14 08:50:53 -0400

    fill out sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,17 @@
 class Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token, only: %i[destroy]
+
+  respond_to :json
+
+  def sign_in_params
+    params.require(:user).permit :email, :password
+  end
+
+  def respond_with(resource, _opts = {})
+    render json: UserSerializer.new(resource).serialized_json, status: :ok
+  end
+
+  def respond_to_on_destroy
+    head :no_content
+  end
 end
