855b700f1f7b334171433993ea7ba0a937669343
Ari Kardasis <ari.kardasis@gmail.com>
2019-03-15 09:54:19 -0400

    add config to facebook omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,7 +308,12 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV.fetch('FACEBOOK_KEY'), ENV.fetch('FACEBOOK_SECRET')
+  config.omniauth :facebook, ENV.fetch('FACEBOOK_KEY'), ENV.fetch('FACEBOOK_SECRET'),{
+    scope: 'email',
+    display: 'popup',
+    info_fields: 'name,email',
+    provider_ignores_state: true
+}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
