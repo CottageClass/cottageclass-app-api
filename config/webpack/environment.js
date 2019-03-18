c7d2525f800b33421498af3895608e2d919fa11d
Ari Kardasis <ari.kardasis@gmail.com>
2019-03-18 12:28:10 -0400

    setup dotenv for use with webpacker and webpack-dev-server

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,20 @@
 const { environment } = require("@rails/webpacker");
 const vue = require("./loaders/vue");
 const custom = require("./custom");
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+const dotenvFiles = [
+  `.env.${process.env.NODE_ENV}.local`,
+  '.env.local',
+  `.env.${process.env.NODE_ENV}`,
+  '.env'
+]
+dotenvFiles.forEach((dotenvFile) => {
+  dotenv.config({ path: dotenvFile, silent: true })
+})
+
+environment.plugins.prepend('Environment', new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(process.env))))
 
 environment.config.merge(custom);
 environment.loaders.append("vue", vue);
