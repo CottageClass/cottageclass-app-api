2c6e1b4e2eb97847a5240fc46756e519755b3c0c
Ari Kardasis <ari.kardasis@gmail.com>
2019-04-08 20:37:53 -0400

    add google maps code.  remove old

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@ import VueAxios from 'vue-axios'
 import VeeValidate from 'vee-validate'
 import VueClipboard from 'vue-clipboard2'
 import VueScrollTo from 'vue-scrollto'
-import * as VueGoogleMaps from 'vue2-google-maps'
 
 import '@babel/polyfill'
 
@@ -15,6 +14,11 @@ import router from '@/router'
 import store from '@/store'
 import '@/registerServiceWorker'
 import VueAnalytics from 'vue-analytics'
+import GoogleMapsLoader from 'google-maps'
+
+GoogleMapsLoader.KEY = process.env.GOOGLE_API_KEY
+GoogleMapsLoader.LIBRARIES = ['geometry', 'places']
+GoogleMapsLoader.load()
 
 let isAuthWindow = false
 try {
@@ -41,30 +45,6 @@ if (isAuthWindow) {
   Vue.use(VueAxios, axios)
   Vue.use(VeeValidate)
   Vue.use(VueClipboard)
-  Vue.use(VueGoogleMaps, {
-    load: {
-      key: 'AIzaSyCAxZ4ERhmcq87C5HK91ujxDLl7gQ_k_-c'
-      // libraries: 'geocoder', // This is required if you use the Autocomplete plugin
-      // OR: libraries: 'places,drawing'
-      // OR: libraries: 'places,drawing,visualization'
-      // (as you require)
-
-      /// / If you want to set the version, you can do so:
-      // v: '3.26',
-    }
-
-    /// / If you intend to programmatically custom event listener code
-    /// / (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
-    /// / instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
-    /// / you might need to turn this on.
-    // autobindAllEvents: false,
-
-    /// / If you want to manually install components, e.g.
-    /// / import {GmapMarker} from 'vue2-google-maps/src/components/marker'
-    /// / Vue.component('GmapMarker', GmapMarker)
-    /// / then disable the following:
-    // installComponents: true,
-  })
 
   Vue.config.productionTip = false
 
@@ -81,7 +61,6 @@ if (isAuthWindow) {
     id: process.env.GOOGLE_ANALYTICS_ID,
     router
   })
-
   document.addEventListener('DOMContentLoaded', () => {
     const element = '#app'
     let token = _.get(document.querySelector(element), 'dataset.token')
