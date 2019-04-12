029372ea0f975f0621c706624f4d53b841ddf125
Ari Kardasis <ari.kardasis@gmail.com>
2019-04-12 09:57:03 -0400

    add new marker component and logic

---
+++ src/components/base/UserPinMapMarker.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="pin-wrapper">
+    <div class="pin-div">
+      <img class="avatar" :src="avatarUrl">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPinMapMarker',
+  props: [ 'avatarUrl' ]
+}
+</script>
+
+<style scoped>
+.pin-wrapper {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+}
+.pin-div {
+  position: absolute;
+  left: -18px;
+  top: -53px;
+  background-image: url('../../assets/pin-blue.svg');
+  background-size: cover;
+  background-repeat:no-repeat;
+  width: 36px;
+  height: 58px;
+}
+.avatar {
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  top: 2px;
+  left: 2px;
+}
+</style>
