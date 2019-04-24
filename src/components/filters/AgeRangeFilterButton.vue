d12506bb348b92437c40e808aab9c6c5e7229c41
holmesworcester <hwilson@gmail.com>
2019-04-24 14:20:51 -0400

    Changes "Ages" back to "All ages" to make it more clear

--- src/components/filters/AgeRangeFilterButton.vue
+++ src/components/filters/AgeRangeFilterButton.vue
@@ -11,7 +11,7 @@ export default {
   computed: {
     buttonText () {
       if (!this.range.data) {
-        return 'Ages'
+        return 'All ages'
       }
       let { min, max } = this.range.data
       if (min < 0) {
@@ -34,7 +34,7 @@ export default {
       if (!min && max) {
         return max + ' & under'
       }
-      return 'Ages'
+      return 'All ages'
     }
   }
 }
