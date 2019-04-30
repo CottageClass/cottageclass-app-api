7c326ddeaa2ee69cbced111245ee5e0dbe81fed5
Ari Kardasis <ari.kardasis@gmail.com>
2019-04-29 21:42:19 -0400

    updated age range selector to use double slider

--- src/components/filters/AgeRangeFilterSelector.vue
+++ src/components/filters/AgeRangeFilterSelector.vue
@@ -1,57 +1,40 @@
 <template>
   <div class="selector-wrapper">
-    <div class="dropdown">
-      <div class="range-dropdown-label">Minimum</div>
-    <select v-model="value.data.min" @change="updateMin">
-      <option value="-1" selected>-</option>
-      <option v-for="age in Array(12).keys()"
-              :key="'min-age-range-' + age"
-              :value="age"
-              >
-              {{age}}
-      </option>
-    </select>
-    </div>
-    <div class="dropdown">
-    <div class="range-dropdown-label">Maximum</div>
-    <select v-model="value.data.max" @change="updateMax">
-      <option value="-1" selected>-</option>
-      <option v-for="age in Array(12).keys()"
-              :key="'max-age-range-' + age"
-              :value="age"
-              >
-              {{age}}
-      </option>
-    </select>
-    </div>
+    <v-range-slider
+      v-model="range"
+      min="0"
+      max="11"
+      @change="update"
+    ></v-range-slider>
+    <div>Ages : {{range[0]}} - {{ range[1]}}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AgeRangeFilter',
+  name: 'AgeRangeFilterSelector',
   props: ['value'],
   data () {
     return {
-      err: null
+      err: null,
+      range: [this.value.data.min, this.value.data.max]
     }
   },
   methods: {
     update () {
+      this.value.data.min = this.range[0]
+      this.value.data.max = this.range[1]
       this.$emit('input', this.value)
-    },
-    updateMax () {
-      if (this.value.data.max >= 0 && this.value.data.min >= 0 && this.value.data.min > this.value.data.max) {
-        this.value.data.min = this.value.data.max
-      }
-      this.update()
-    },
-
-    updateMin () {
-      if (this.value.data.min >= 0 && this.value.data.max >= 0 && this.value.data.min > this.value.data.max) {
-        this.value.data.max = this.value.data.min
-      }
-      this.update()
+    }
+  },
+  created () {
+    this.range = [this.value.data.min >= 0 ? this.value.data.min : 0,
+      this.value.data.max >= 0 ? this.value.data.max : 11]
+  },
+  watch: {
+    value () {
+      this.range = [this.value.data.min >= 0 ? this.value.data.min : 0,
+        this.value.data.max >= 0 ? this.value.data.max : 11]
     }
   }
 }
