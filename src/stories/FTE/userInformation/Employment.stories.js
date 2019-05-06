8c4fc0fe28b091784ff5e70796cf534ac8ca0a71
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-06 14:40:22 -0400

    fix story

--- src/stories/FTE/userInformation/Employment.stories.js
+++ src/stories/FTE/userInformation/Employment.stories.js
@@ -8,13 +8,28 @@ Vue.component('Employment', Employment)
 storiesOf('Employment', module)
   .add('with no existing info', () => ({
     components: { Employment },
-    template: '<Employment jobPosition="" employer=""/>'
+    data () {
+      return {
+        val: { jobPosition: '', employer: '' }
+      }
+    },
+    template: '<Employment v-model="val"/>'
   }))
   .add('with some data in the onboarding style', () => ({
     components: { Employment, StyleWrapper },
-    template: '<StyleWrapper styleIs="onboarding"> <Employment jobPosition="Master" employer="universe"/></StyleWrapper>'
+    data () {
+      return {
+        val: { jobPosition: 'Master', employer: 'Univers' }
+      }
+    },
+    template: '<StyleWrapper styleIs="onboarding"> <Employment v-model="val"/></StyleWrapper>'
   }))
   .add('with some data in the editing style', () => ({
     components: { Employment, StyleWrapper },
-    template: '<StyleWrapper styleIs="editing"> <Employment jobPosition="Master" employer="universe"/></StyleWrapper>'
+    data () {
+      return {
+        val: { jobPosition: 'Master', employer: 'Univers' }
+      }
+    },
+    template: '<StyleWrapper styleIs="editing"> <Employment v-model="val"/></StyleWrapper>'
   }))
