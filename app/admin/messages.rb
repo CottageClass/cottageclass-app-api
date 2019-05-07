4cc069e24f39c25c65c13d8f92be5623408b0b3a
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-07 16:47:28 -0400

    added link to twilio messages from active admin and removed some unnecessary columns

--- app/admin/messages.rb
+++ app/admin/messages.rb
@@ -3,4 +3,19 @@ ActiveAdmin.register Message do
 
   menu priority: 2
   permit_params :content
+  index do
+    column :id
+    column :sender
+    column :receiver
+    column :content
+    column :created_at
+    column :updated_at
+    column 'link' do |message|
+      service_id = ENV['TWILIO_PROXY_SERVICE_SID']
+      session_id = message.cc_twilio_session.twilio_sid
+      link_to('LINK',
+              "https://www.twilio.com/console/proxy/services/#{service_id}/sessions/#{session_id}/interactions",
+              target: :_blank)
+    end
+  end
 end
