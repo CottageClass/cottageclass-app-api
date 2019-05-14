53cfb88ac04dc4759b9ea9a2ab8793c60525f387
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-14 09:44:18 -0400

    facebook button component with basic test

---
+++ src/components/base/FacebookButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div
+    class="fb-button w-inline-block"
+    @click.prevent="authenticateFacebook"
+    >
+      <img src="@/assets/facebook-button-icon.svg" width="24" height="24" alt="">
+      <div class="fb-button-text">Continue with Facebook</div>
+    </div>
+</template>
+
+<script>
+import { providerAuthentication } from '@/mixins'
+
+export default {
+  name: 'FacebookButotn',
+  mixins: [ providerAuthentication ]
+}
+</script>
+
+<style scoped>
+
+</style>
