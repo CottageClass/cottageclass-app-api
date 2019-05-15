6eae6767a8c66b159bc86241d9ac1d83109a15bd
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-14 21:23:04 -0400

    ensure star uniqueness in table

--- db/migrate/20190514144821_create_stars.rb
+++ db/migrate/20190514144821_create_stars.rb
@@ -3,7 +3,9 @@ class CreateStars < ActiveRecord::Migration[5.2]
     create_table :stars do |t|
       t.references :starable, polymorphic: true
       t.references :giver, foreign_key: {to_table: :users}
-
+      t.index %i[giver_id starable_type starable_id],
+              unique: true,
+              name: :index_stars_on_starable_type_starable_id_giver_id
       t.timestamps
     end
   end
