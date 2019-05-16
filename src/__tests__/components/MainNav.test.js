bb392c5b0b554b9e0c2cc24022648c70bd45bc1f
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-16 09:53:58 -0400

    add link to main nav

---
+++ src/__tests__/components/MainNav.test.js
@@ -0,0 +1,59 @@
+import { shallowMount } from '@vue/test-utils'
+import MainNav from '@/components/MainNav.vue'
+
+describe('MainNav', () => {
+  let wrapper
+
+  describe('unauthenticated', () => {
+    beforeAll(async () => {
+      const $store = {
+        getters: {
+          currentUser: null,
+          isAuthenticated: false
+        }
+      }
+      wrapper = shallowMount(MainNav, {
+        stubs: ['router-link'],
+        mocks: { $store }
+      })
+    })
+
+    it('has correct  list items', done => {
+      const dropdownButton = wrapper.find('.nav-menu-button')
+      dropdownButton.trigger('click')
+      wrapper.vm.$nextTick(() => {
+        const list = wrapper.find('.nav-links-expanded ul')
+        const itemLabels = list.findAll('li div').wrappers.map(w => w.text())
+        expect(itemLabels).toMatchSnapshot()
+        done()
+      })
+    })
+  })
+  describe('authenticated', () => {
+    beforeAll(async () => {
+      const $store = {
+        getters: {
+          currentUser: null,
+          isAuthenticated: true
+        }
+      }
+      wrapper = shallowMount(MainNav, {
+        stubs: ['router-link'],
+        mocks: { $store }
+      })
+    })
+
+    it('has correct  list items', done => {
+      const dropdownButton = wrapper.find('.nav-menu-button')
+      dropdownButton.trigger('click')
+      wrapper.vm.$nextTick(() => {
+        const list = wrapper.find('.nav-links-expanded ul')
+        const itemLabels = list.findAll('li div').wrappers.map(w => w.text())
+        console.log(itemLabels)
+
+        expect(itemLabels).toMatchSnapshot()
+        done()
+      })
+    })
+  })
+})
