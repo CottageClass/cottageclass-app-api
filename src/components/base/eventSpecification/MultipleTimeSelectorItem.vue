7ad9eeb94669950dbce8e285a4b1ca630d0e99dd
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-20 12:46:09 -0400

    time selector item and test

---
+++ src/components/base/eventSpecification/MultipleTimeSelectorItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="item-container" @click="toggleSelected">
+    <div class="time-label">
+      {{ timeText }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { } from '@/mixins'
+
+export default {
+  name: 'MultipleTimeSelectorItem',
+  props: ['startHour', 'selected'],
+  components: {},
+  mixins: [],
+  data () {
+    return {
+    }
+  },
+  computed: {
+    timeText () {
+      return this.toCompactTime(this.startHour) + ' - ' + this.toCompactTime((this.startHour + 1) % 24)
+    },
+    toCompactTime () {
+      return hour => (hour + 11) % 12 + 1 + (hour < 12 ? 'am' : 'pm')
+    },
+    ...mapGetters([])
+  },
+  methods: {
+    toggleSelected () {
+      this.$emit('input', !this.selected)
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
