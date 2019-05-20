542aff3f125038c4f5800a87e32291a7081173c6
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-20 13:25:42 -0400

    beginning of time selector component

---
+++ src/components/base/eventSpecification/MultipleTimeSelector.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="day-of-week"
+         v-for="(day, index) of shiftedDays">
+      {{ day  +index }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { } from '@/mixins'
+
+const WEEK_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+export default {
+  name: 'MultipleTimeSelector',
+  props: ['selectedItems', 'firstDisplay'],
+  components: {},
+  mixins: [],
+  data () {
+    return {
+    }
+  },
+  computed: {
+    firstDay () {
+      return Math.floor(this.firstDisplay / 24)
+    },
+    firstTime () {
+      return Math.max(8, this.firstDisplay % 24)
+    },
+    shiftedDays () {
+      return [0, 1, 2, 3, 4, 5, 6, 7].map(i => WEEK_DAYS[(i + this.firstDay) % 7])
+    },
+    ...mapGetters([])
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
