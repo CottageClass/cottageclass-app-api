167e0cfbef5afdfe123c7257b1b1d234de3f9781
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-22 15:43:26 -0400

    complete migration

--- db/migrate/20190522194051_add_house_info_to_users.rb
+++ db/migrate/20190522194051_add_house_info_to_users.rb
@@ -1,4 +1,7 @@
 class AddHouseInfoToUsers < ActiveRecord::Migration[5.2]
   def change
+    add_column :users, :has_pets, :boolean
+    add_column :users, :pet_description, :text
+    add_column :users, :house_rules, :text
   end
 end
