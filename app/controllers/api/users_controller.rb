c2eac8651d1cacb297d8a12a6425791ebfd0bc70
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-29 12:47:35 -0400

    main logic block

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -13,6 +13,43 @@ class API::UsersController < API::BaseController
                 path: proc { |**parameters| index_api_users_path parameters }
   end
 
+  def feed
+    users = User
+    page = params[:page]
+    page_size = params[:page_size]
+    miles = params[:miles]
+    latitude = params[:latitude]
+    longitude = params[:longitude]
+
+    miles = miles.to_f
+    if miles.positive?
+      location = []
+      location = [latitude, longitude] if [latitude, longitude].all?(&:present?)
+      location = [current_user.latitude, current_user.longitude] if location.blank? && current_user.present?
+      users = users.near(location.map(&:to_f), miles) if location.all?(&:present?)
+    end
+    users = users.where.not(id: current_user.id) if current_user
+
+    users = users.joins 'LEFT JOIN events ON users.showcase_event_id = events.id'
+    users = users.reorder 'events.recency_score DESC NULLS LAST, distance ASC'
+
+    links = {}
+    meta = { users_count: users.count(:all) }
+    if page.present?
+      page_size ||= 10
+      users = users.page(page).per page_size
+
+      meta[:pages_count] = users.total_pages
+      links[:self] = path.call page: users.current_page, page_size: page_size
+      links[:previous] = path.call(page: users.prev_page, page_size: page_size) unless users.first_page?
+      links[:next] = path.call(page: users.next_page, page_size: page_size) unless users.last_page?
+    end
+    serializer = PublicUserSerializer.new users, include: %i[children showcase_event],
+                                                 links: links,
+                                                 meta: meta
+    render json: serializer.serializable_hash, status: :ok
+  end
+
   def show
     serializer = if current_user && current_user.id == @user.id
                    CurrentUserSerializer.new @user, include: %i[children]
@@ -25,12 +62,12 @@ class API::UsersController < API::BaseController
   private
 
   def users_index(users:, miles:, latitude:, longitude:, page:, page_size:, path:, min_age:, max_age:)
-    if min_age.present? or max_age.present?
-      min_age ||=  0
+    if min_age.present? || max_age.present?
+      min_age ||= 0
       min_age = min_age.to_i
       max_age ||= 17
       max_age = max_age.to_i
-      earliest_birthday = (Time.current - (max_age+ 1 ).year.seconds)
+      earliest_birthday = (Time.current - (max_age + 1).year.seconds)
       latest_birthday = (Time.current - min_age.year.seconds)
       time_range = earliest_birthday..latest_birthday
 
