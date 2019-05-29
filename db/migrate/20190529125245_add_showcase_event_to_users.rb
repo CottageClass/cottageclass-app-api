726b4cc646ab4b91858651aa5bae32ae7132cce8
Ari Kardasis <ari.kardasis@gmail.com>
2019-05-29 11:53:37 -0400

    updated migration

--- db/migrate/20190529125245_add_showcase_event_to_users.rb
+++ db/migrate/20190529125245_add_showcase_event_to_users.rb
@@ -1,5 +1,5 @@
 class AddShowcaseEventToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_reference :users, :showcase_event, foreign_key: true
+    add_reference :users, :showcase_event, foreign_key: { to_table: :events }
   end
 end
