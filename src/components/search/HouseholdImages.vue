adfedfe059343f3aa85fd321a9f30e1e49241327
Ari Kardasis <ari.kardasis@gmail.com>
2019-06-05 16:01:10 -0400

    add household images

---
+++ src/components/search/HouseholdImages.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="household-photos-tiny--container">
+    <img v-for="imageUrl of displayImageUrls"
+         :src="imageUrl" width="24" height="24"
+         alt="" class="household-photos-tiny__photo photo-fit" />
+    <div class="other-events-card__attendees--more">12 more</div>
+  </div>
+</template>
+
+<script>
+
+import { householdImageUrl } from '@/utils/cloudinary'
+
+export default {
+  name: 'HouseholdImages',
+  props: { user: { required: true } },
+  mixins: [],
+  data () {
+    return {
+    }
+  },
+  computed: {
+    displayImageUrls () {
+      return this.user.images.slice(0, 3).map(url => {
+        return householdImageUrl(url, 24)
+      })
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
