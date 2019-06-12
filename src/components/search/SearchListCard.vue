7081f6466608bbed95f51adeaf3122d1db37f430
Ari Kardasis <ari.kardasis@gmail.com>
2019-06-12 13:40:36 -0400

    skip occupation if not present

--- src/components/search/SearchListCard.vue
+++ src/components/search/SearchListCard.vue
@@ -24,7 +24,10 @@
       <div class="user-info--container">
         <div class="user-info_list">
           <div class="user-info__name">{{userName}}</div>
-          <div class="user-info__occupation lp-truncate">{{occupation}}<br /></div>
+          <div v-if="occuption"
+               class="user-info__occupation lp-truncate">
+            l{{occupation}}<br />
+          </div>
           <div class="user-info__kids lp-truncate">{{kidsAges}}</div>
           <HouseholdImages :user="user" />
         </div>
