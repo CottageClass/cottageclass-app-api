dbf55dfc0d4c733d549c37d8321c06ae5a60616e
Ari Kardasis <ari.kardasis@gmail.com>
2019-06-13 09:06:33 -0400

    move distanceHaversine out of api

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,8 +13,6 @@ export function distanceHaversine (lat1, lon1, lat2, lon2) {
   const unit = 'N' // always return miles
   var radlat1 = Math.PI * lat1 / 180
   var radlat2 = Math.PI * lat2 / 180
-  // var radlon1 = Math.PI * lon1 / 180
-  // var radlon2 = Math.PI * lon2 / 180
   var theta = lon1 - lon2
   var radtheta = Math.PI * theta / 180
   var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
