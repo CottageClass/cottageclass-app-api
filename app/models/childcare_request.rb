86e242956d802ebdd86c36f257d5e9fde0d2f3df
Ari Kardasis <ari.kardasis@gmail.com>
2019-07-03 09:56:05 -0400

    create SearchListItem for every childcare request

--- app/models/childcare_request.rb
+++ app/models/childcare_request.rb
@@ -1,5 +1,12 @@
 class ChildcareRequest < ApplicationRecord
   belongs_to :user, inverse_of: :childcare_requests
+  after_create :create_search_list_item
 
   attribute :content
+
+  has_one :search_list_item, as: :itemable, class_name: 'SearchListItem', dependent: :destroy
+
+  def create_search_list_item
+    SearchListItem.create(user: user, itemable: self)
+  end
 end
