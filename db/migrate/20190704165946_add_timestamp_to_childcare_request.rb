5bdbfb9b3c75e025185fc3a28ef6a638e921ceb2
Ari Kardasis <ari.kardasis@gmail.com>
2019-07-04 13:23:57 -0400

    add created_at column to ccr's

--- db/migrate/20190704165946_add_timestamp_to_childcare_request.rb
+++ db/migrate/20190704165946_add_timestamp_to_childcare_request.rb
@@ -1,5 +1,5 @@
 class AddTimestampToChildcareRequest < ActiveRecord::Migration[5.2]
   def change
-    add_column :childcare_requests, :created_at, :datetime, null: false
+    add_column :childcare_requests, :created_at, :datetime, null: false, default: -> { 'CURRENT_TIMESTAMP' }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_142542) do
+ActiveRecord::Schema.define(version: 2019_07_04_165946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_142542) do
   create_table "childcare_requests", force: :cascade do |t|
     t.bigint "user_id"
     t.text "content"
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.index ["user_id"], name: "index_childcare_requests_on_user_id"
   end
 
