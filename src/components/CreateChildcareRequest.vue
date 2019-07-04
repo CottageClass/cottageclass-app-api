31464bee12d2f6528a4d10e21ba97b7471db0431
Ari Kardasis <ari.kardasis@gmail.com>
2019-07-04 09:22:26 -0400

    show alert after requst is submitted instead of share page

--- src/components/CreateChildcareRequest.vue
+++ src/components/CreateChildcareRequest.vue
@@ -15,11 +15,6 @@
         placeholder="Describe your playdate here, e.g. 'Come over and play at our house!'"
         v-model="description.text" />
     </Question>
-    <Question v-if="stepName==='success'"
-      title="Describe your request"
-      subtitle="What are you looking for? For what times? What kinds of needs does your child have? Parents near you will see your request and reply by text message. We strongly recommend meeting them for a playdate first!">
-    share button
-    </Question>
   </div>
 </template>
 
@@ -28,14 +23,14 @@ import Nav from '@/components/FTE/Nav'
 import Question from '@/components/base/Question.vue'
 import FormWithTextArea from '@/components/base/FormWithTextArea.vue'
 import ErrorMessage from '@/components/base/ErrorMessage.vue'
-import { stepNavigation } from '@/mixins'
+import { stepNavigation, alerts } from '@/mixins'
 import { submitChildcareRequest } from '@/utils/api'
 
 export default {
   name: 'CreateChildcareRequest',
   components: { Question, FormWithTextArea, Nav, ErrorMessage },
   props: ['stepName'],
-  mixins: [stepNavigation],
+  mixins: [stepNavigation, alerts],
   data () {
     return {
       showError: false,
@@ -47,33 +42,30 @@ export default {
       return this.description.err
     },
     nextButtonState () {
-      if (this.stepName === 'success') {
-        return 'done'
-      } else if (this.errorMessage) {
+      if (this.errorMessage) {
         return 'inactive'
       } else {
         return 'next'
       }
     },
     stepSequence () {
-      return ['description', 'success']
+      return ['description']
     }
   },
   methods: {
-    nextStep () {
+    async nextStep () {
       if (this.nextButtonState === 'skip') {
         this.$emit('skip')
       } else if (this.errorMessage) {
         this.showError = true
       } else {
-        // state is persisted after route update because component is reused
         this.showError = false
-        if (this.stepName === 'description') {
-          submitChildcareRequest(this.description.text)
-          this.$router.push({
-            params: { stepName: this.stepSequence[this.stepIndex + 1] }
-          })
-        } else {
+        try {
+          await submitChildcareRequest(this.description.text)
+          this.showAlertOnNextRoute('Your request has been submitted', 'success')
+        } catch (e) {
+          this.showAlertOnNextRoute('There was a problem submitting your request, please try again later', 'failure')
+        } finally {
           this.$router.push({ name: 'Search' })
         }
       }
