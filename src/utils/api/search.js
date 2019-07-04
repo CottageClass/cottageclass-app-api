ab01286cbfac6309eb804d3fc4a77c556a282aea
Ari Kardasis <ari.kardasis@gmail.com>
2019-07-04 11:45:43 -0400

    fixed problem with loading user items

--- src/utils/api/search.js
+++ src/utils/api/search.js
@@ -15,11 +15,11 @@ export async function fetchFeed ({ miles, lat, lng, minAge, maxAge, pageSize = 2
     }
     url += `page/${page}/page_size/${pageSize}`
     const res = await axios.get(url)
+    logger.log('FETCH USERS WITHIN DISTANCE SUCCESS')
+    logger.log(res.data)
     const normalizedData = normalize(res.data, { endpoint: 'item' })
     logger.debug({ normalizedData })
     return createItems(normalize(res.data, { endpoint: 'item' }))
-    logger.log('FETCH USERS WITHIN DISTANCE SUCCESS')
-    logger.log(res.data)
   } catch (err) {
     logger.logError('FETCH USERS WITHIN DISTANCE FAILURE')
     logger.logError(err)
--- src/utils/createItem.js
+++ src/utils/createItem.js
@@ -1,19 +1,22 @@
+import Logger from '@/utils/logger'
+
 import { createUsers } from '@/utils/createUser'
 import { createEvents } from '@/utils/createEvent'
 import { createChildcareRequests } from '@/utils/createChildcareRequest'
+
+const logger = Logger('createItem')
 
 export const createItem = (data) => {
   return createItems(data)[0]
 }
 
 export const createItems = (data) => {
-  console.log({ data })
   const users = createUsers(data)
   const events = createEvents(data)
   const childcareRequests = createChildcareRequests(data)
 
   const itemIds = Object.keys(data.searchListItem)
-  console.log({ users, events, childcareRequests, itemIds })
+
   const items = itemIds.map(itemId => {
     const item = data.searchListItem[itemId]
     const res = { id: itemId }
@@ -21,18 +24,20 @@ export const createItems = (data) => {
       return u.id === item.relationships.user.data.id
     })
     Object.assign(res, { user })
-    if (item.relationships.itemable.data.type === 'event') {
-      const event = events.find(e => e.id === item.relationships.itemable.data.id)
-      Object.assign(res, { event })
-    } else if (item.relationships.itemable.data.type === 'childcareRequest') {
-      const childcareRequest = childcareRequests.find(cr => cr.id === item.relationships.itemable.data.id)
-      Object.assign(res, { childcareRequest })
+    if (item.relationships.itemable.data) {
+      if (item.relationships.itemable.data.type === 'event') {
+        const event = events.find(e => e.id === item.relationships.itemable.data.id)
+        Object.assign(res, { event })
+      } else if (item.relationships.itemable.data.type === 'childcareRequest') {
+        const childcareRequest = childcareRequests.find(cr => cr.id === item.relationships.itemable.data.id)
+        Object.assign(res, { childcareRequest })
+      }
     }
+    logger.log('items created')
+    logger.log(res)
     return res
   })
   if (data.meta.item) {
-    console.log('data.meta.item')
-    console.log(data.meta.item)
     const order = data.meta.item.data.map(u => u.id)
     return order.map(id => items.find(i => i.id === id))
   }
