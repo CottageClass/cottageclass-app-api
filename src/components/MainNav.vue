cfc69021d9d5516e5837d70155feac2113d3a154
Ari Kardasis <ari.kardasis@gmail.com>
2019-08-01 15:24:17 -0400

    remove mapped mutations

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -49,7 +49,7 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters } from 'vuex'
 import { mixin as clickaway } from 'vue-clickaway'
 
 import MainNavLogo from '@/components/MainNavLogo'
@@ -85,14 +85,13 @@ export default {
       this.showMenu = false
     },
     closeCallback () {
-      this.hideModal()
+      this.$store.commit('hideModal')
       this.$router.push({ query: {} })
-    },
-    ...mapMutations(['showModal', 'hideModal'])
+    }
   },
   mounted () {
     if (this.$route.query && this.$route.query['welcome-to-the-new-lilypad']) {
-      this.showModal({
+      this.$store.commit('showModal', {
         modal: {
           title: `What's in a name? KidsClub is now Lilypad!`,
           bodyText: 'Hi! We just changed our name to Lilypad (and moved our site to JoinLilypad.com) but everything about the service, the people, and the organization behind it is exactly the same. \n\nWe hope you love the new name (and lilypads) as much as we do, and we hope you find some great new parent friends, fun playdates, and childcare swaps on Lilypad!',
