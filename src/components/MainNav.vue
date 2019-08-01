5da3c76909a1b655ba9145e33f2429bf1168222c
Ari Kardasis <ari.kardasis@gmail.com>
2019-08-01 14:44:10 -0400

    typo

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -95,7 +95,7 @@ export default {
       this.showModal({
         modal: {
           title: `What's in a name? KidsClub is now Lilypad!`,
-          bodyText: 'Hi! We just changed our name to LilyPad (and moved our site to JoinLilypad.com) but everything about the service, the people, and the organization behind it is exactly the same. \n\nWe hope you love the new name (and lilypads) as much as we do, and we hope you find some great new parent friends, fun playdates, and childcare swaps on Lilypad!',
+          bodyText: 'Hi! We just changed our name to Lilypad (and moved our site to JoinLilypad.com) but everything about the service, the people, and the organization behind it is exactly the same. \n\nWe hope you love the new name (and lilypads) as much as we do, and we hope you find some great new parent friends, fun playdates, and childcare swaps on Lilypad!',
           buttonNames: ['Continue to JoinLilypad.com >'],
           closeCallback: this.closeCallback
         }
