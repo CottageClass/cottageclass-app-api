6d66e0805bedfd852c0def99817ab42e60d4a499
Ari Kardasis <ari.kardasis@gmail.com>
2019-08-22 17:04:14 -0400

    show larger photos

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="mask">
-    <div class="scrolling-wrapper">
-      <div class="carousel-image-container" v-for="image in transformedImages">
-        <img :src="image">
-      </div>
-    </div>
+  <div class="hh-photos__grid">
+    <a v-for="image in transformedImages" class="hh-photo__link w-inline-block">
+      <img :src="image" class="hh-photos__image photo-fit" />
+    </a>
   </div>
 </template>
 
@@ -15,48 +13,76 @@ export default {
   props: ['images'],
   computed: {
     transformedImages () {
-      return this.images.map(url => imageUrl(url, { height: 111 }))
+      return this.images.map(url => imageUrl(url, { height: 200 }))
     }
   }
 }
 </script>
 
-<style scoped>
-
-.mask {
-  height: 123px;
-  overflow: hidden;
-  width: 100%;
+<style scoped lang="scss">
+a {
+  color: #000;
+  text-decoration: none;
 }
-.scrolling-wrapper {
-  overflow-x: scroll;
-  overflow-y: hidden;
+
+.hh-photos__grid {
+  position: relative;
   display: flex;
-  flex-wrap: nowrap;
-  align-items: center;
-  background-color: #fff;
-  -webkit-overflow-scrolling: touch;
-
-  padding-bottom: 20px;
-  padding-top: 6px;
-  box-sizing: content-box;
+  margin: 20px -12px -2px -2px;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  align-items: flex-start;
 }
 
-.carousel-image-container {
-  flex-shrink: 0;
-  width: 111px;
-  height: 111px;
-  margin-right: 6px;
-  background-color: #fff;
+.hh-photo__link {
+  position: relative;
+  overflow: hidden;
+  width: 32%;
+  margin: 2px;
+  padding-top: 33%;
+  background-color: #d59191;
 }
 
-.carousel-image-container img {
-  width: 111px;
-  height: 111px;
+.hh-photos__image {
   object-fit: cover;
-  width: auto;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
   height: 100%;
-  background-color: #fff;
 }
 
+@media (max-width: 991px){
+  .hh-photos__grid {
+    margin-right: -17px;
+    margin-bottom: 2px;
+  }
+
+}
+
+@media (max-width: 767px){
+  .hh-photos__grid {
+    margin-right: -8px;
+    justify-content: flex-start;
+  }
+
+  .hh-photo__link {
+    overflow: hidden;
+    width: 49%;
+    padding-top: 50%;
+  }
+
+}
+
+@media (max-width: 479px){
+  .hh-photos__grid {
+    margin-right: -8px;
+  }
+
+  .hh-photo__link {
+    width: 48%;
+  }
+
+}
 </style>
