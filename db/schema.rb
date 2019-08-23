29f9aee08571179eed80ffc17e5aa485d910b903
Ari Kardasis <ari.kardasis@gmail.com>
2019-08-23 13:22:02 -0400

    add KeyValue gem for storing app data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_174041) do
+ActiveRecord::Schema.define(version: 2019_08_23_172033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,6 +140,12 @@ ActiveRecord::Schema.define(version: 2019_08_12_174041) do
     t.float "recency_score"
     t.index ["event_series_id"], name: "index_events_on_event_series_id"
     t.index ["latitude", "longitude"], name: "index_events_on_latitude_and_longitude"
+  end
+
+  create_table "key_values", force: :cascade do |t|
+    t.string "key", null: false
+    t.text "value", null: false
+    t.index ["key"], name: "index_key_values_on_key", unique: true
   end
 
   create_table "messages", force: :cascade do |t|
