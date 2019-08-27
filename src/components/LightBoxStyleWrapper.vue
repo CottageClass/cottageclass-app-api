f75f662d08814a0d2005024067fd60b74dd2aee9
holmesworcester <hwilson@gmail.com>
2019-08-27 18:55:52 -0400

    Adds white style

--- src/components/LightBoxStyleWrapper.vue
+++ src/components/LightBoxStyleWrapper.vue
@@ -13,14 +13,20 @@ export default {
 <style lang="scss">
 
 .lightbox-style {
+  svg {
+    fill: rgba(0, 0, 0, 0.7);
+  }
+  .vue-lb-footer-count {
+    color: #333;
+  }
   .vue-lb-container {
     background-color: white;
     color: white;
     }
+  .vue-lb-modal-thumbnail-active {
+    box-shadow: inset 0 0 0 2px #fd6f77
+  }
 
-  .vue-lb-button-close {
-    display: none;
-    }
 }
 
 </style>
