091f257c673de8e9b1261648cf21108000623985
Ari Kardasis <ari.kardasis@gmail.com>
2019-08-27 17:56:39 -0400

    remove dead routes

--- config/routes.rb
+++ config/routes.rb
@@ -83,10 +83,6 @@ Rails.application.routes.draw do
       max_age: /\d+/,
       as: :feed
 
-  get '/users/:sender_id/messages/:receiver_id' => 'messages#admin_for_pair', as: 'messages_for_pair'
-
-  get '/users' => 'users#admin_index', as: 'admin_all_users'
-
   # twilio sessions
   post '/api/users/:id/proxy_sessions' => 'twilio_sessions#create', as: 'proxy_sessions'
   post '/proxy_callback' => 'twilio_sessions#callback'
