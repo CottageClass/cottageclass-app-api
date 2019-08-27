c18bb93a161bdc79bca7697cfcb4096c2ff24160
holmesworcester <hwilson@gmail.com>
2019-08-27 18:33:35 -0400

    Adds wrapper to control lightbox style

---
+++ src/components/LightBoxStyleWrapper.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="lightbox-style">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'LightBoxStyleWrapper'
+}
+</script>
+
+<style lang="scss">
+
+.lightbox-style {
+  .vue-lb-container {
+    background-color: white;
+    color: white;
+    }
+
+  .vue-lb-button-close {
+    display: none;
+    }
+}
+
+</style>
