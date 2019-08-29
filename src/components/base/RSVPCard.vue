650653123f3781e0825b297ec684e74b5d3171f7
Ari Kardasis <ari.kardasis@gmail.com>
2019-08-29 12:29:44 -0400

    add new interested cards

---
+++ src/components/base/RSVPCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <InterestedCard
+    title="Can you attend?"
+    otherText="Suggest another time"
+    @primary-click="$emit('rsvp-yes')"
+    @secondary-click="$emit('rsvp-no')"
+    @other-click="$emit('other-time-click')"
+  />
+</template>
+
+<script>
+import InterestedCard from '@/components/base/InterestedCard'
+export default {
+  name: 'RSVPCard',
+  components: { InterestedCard }
+}
+</script>
