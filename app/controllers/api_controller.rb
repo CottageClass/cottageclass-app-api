ad22f0ffe5670c4de9dfad6cfded960bbfa4426b
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-05 11:43:27 -0400

    inventory control

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,2 @@
 class ApiController < ActionController::API
-  ADMIN_WHITELIST = [
-    'holmes@cottageclass.com',
-    'manisha@cottageclass.com',
-    'developer@cottageclass.com',
-    'asa@cottageclass.com'
-  ].freeze
-
-  def require_admin!
-    redirect_to root_path unless ADMIN_WHITELIST.include?(current_user.email)
-  end
 end
