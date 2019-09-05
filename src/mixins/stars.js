2eadcf69da90a43c3497bdd578267f04424dc99b
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-05 16:08:36 -0400

    track stars

--- src/mixins/stars.js
+++ src/mixins/stars.js
@@ -1,28 +1,45 @@
 import { unstarUser, starUser, darkStarUser, undarkStarUser } from '@/utils/api'
+import { trackEvent } from '@/utils/vendor/ahoy'
 
 export default {
   methods: {
     starUser (userId, context) {
       return starUser(userId).then(res => {
         this.$ga.event('Star', 'added - ' + context, userId)
+        trackEvent('star_added', {
+          context,
+          userId
+        })
         return res
       })
     },
     unstarUser (userId, context) {
       return unstarUser(userId).then(res => {
         this.$ga.event('Star', 'removed - ' + context, userId)
+        trackEvent('star_removed', {
+          context,
+          userId
+        })
         return res
       })
     },
     darkStarUser (userId, context) {
       return darkStarUser(userId).then(res => {
         this.$ga.event('DarkStar', 'added - ' + context, userId)
+        trackEvent('dark_star_added', {
+          context,
+          userId
+        })
         return res
       })
     },
     undarkStarUser (userId, context) {
       return undarkStarUser(userId).then(res => {
         this.$ga.event('DarkStar', 'removed - ' + context, userId)
+        trackEvent('dark_star_removed', {
+          context,
+          userId
+        })
         return res
       })
     }
