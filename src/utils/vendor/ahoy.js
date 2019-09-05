2bade8ae0d0fc677b828e536ca6797873ec52a9d
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-05 16:08:17 -0400

    update trackEvent method

--- src/utils/vendor/ahoy.js
+++ src/utils/vendor/ahoy.js
@@ -1,6 +1,5 @@
 import ahoy from 'ahoy.js'
 
-export function trackEvent ({ name, properties }) {
-  console.log('hji')
+export function trackEvent (name, properties) {
   ahoy.track(name, properties)
 }
