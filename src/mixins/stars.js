023887f7649a58eb332ad9077da406a6999fd52c
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-05 17:01:05 -0400

    convert all the star methods to async/await

--- src/mixins/stars.js
+++ src/mixins/stars.js
@@ -3,45 +3,53 @@ import { trackEvent } from '@/utils/vendor/ahoy'
 
 export default {
   methods: {
-    starUser (userId, context) {
-      return starUser(userId).then(res => {
-        this.$ga.event('Star', 'added - ' + context, userId)
-        trackEvent('star_added', {
+    async starUser (userId, context) {
+      try {
+        await starUser(userId)
+        await this.$ga.event('Star', 'added - ' + context, userId)
+        await trackEvent('star_added', {
           context,
           userId
         })
-        return res
-      })
+      } catch (err) {
+        throw err
+      }
     },
-    unstarUser (userId, context) {
-      return unstarUser(userId).then(res => {
-        this.$ga.event('Star', 'removed - ' + context, userId)
-        trackEvent('star_removed', {
+    async unstarUser (userId, context) {
+      try {
+        await unstarUser(userId)
+        await this.$ga.event('Star', 'removed - ' + context, userId)
+        await trackEvent('star_removed', {
           context,
           userId
         })
-        return res
-      })
+      } catch (err) {
+        throw err
+      }
     },
-    darkStarUser (userId, context) {
-      return darkStarUser(userId).then(res => {
-        this.$ga.event('DarkStar', 'added - ' + context, userId)
-        trackEvent('dark_star_added', {
+    async darkStarUser (userId, context) {
+      try {
+        await darkStarUser(userId)
+        await this.$ga.event('DarkStar', 'added - ' + context, userId)
+        await trackEvent('dark_star_added', {
           context,
           userId
         })
-        return res
-      })
+      } catch (err) {
+        throw err
+      }
     },
-    undarkStarUser (userId, context) {
-      return undarkStarUser(userId).then(res => {
-        this.$ga.event('DarkStar', 'removed - ' + context, userId)
-        trackEvent('dark_star_removed', {
+    async undarkStarUser (userId, context) {
+      try {
+        await undarkStarUser(userId)
+        await this.$ga.event('DarkStar', 'removed - ' + context, userId)
+        await trackEvent('dark_star_removed', {
           context,
           userId
         })
-        return res
-      })
+      } catch (err) {
+        throw err
+      }
     }
   }
 }
