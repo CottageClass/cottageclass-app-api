ff97073172af5b0b9b4ec2dc058654d51d0168bb
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-06 11:29:41 -0400

    better log for now

--- src/utils/ahoy.js
+++ src/utils/ahoy.js
@@ -3,6 +3,7 @@ import Logger from '@/utils/logger'
 const logger = Logger('ahoy')
 
 export async function trackEvent (name, properties) {
-  logger.log('tracking event', { name, properties })
+  logger.debug('tracking event')
+  logger.debug({ name, properties })
   return ahoy.track(name, properties)
 }
