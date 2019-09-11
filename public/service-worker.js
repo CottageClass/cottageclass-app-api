12b4b3b30729004cefac446fd4806cf004ddd988
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-11 10:47:52 -0400

    some dummy push handling

--- public/service-worker.js
+++ public/service-worker.js
@@ -46,13 +46,35 @@ if (workbox) {
     })
   )
 
-  self.addEventListener('push', (event) => {
-    console.log('push notification received')
-    const title = 'Get Started With Workbox'
-    const options = {
-      body: event.data.text()
+  self.addEventListener('push', function (e) {
+    var body
+
+    if (e.data) {
+      body = e.data.text()
+    } else {
+      body = 'Push message no payload'
     }
-    event.waitUntil(self.registration.showNotification(title, options))
+
+    var options = {
+      body: body,
+      icon: 'images/notification-flat.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      },
+      actions: [
+        { action: 'explore',
+          title: 'Explore this new world',
+          icon: 'images/checkmark.png' },
+        { action: 'close',
+          title: 'I don\'t want any of this',
+          icon: 'images/xmark.png' }
+      ]
+    }
+    e.waitUntil(
+      self.registration.showNotification('Push Notification', options)
+    )
   })
 } else {
   console.log(`Boo! Workbox didn't load 😬`)
