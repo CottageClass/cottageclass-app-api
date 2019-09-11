74d3f138d44ab42393defe85dad27d35378f70b2
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-11 16:49:38 -0400

    verify existence of Notification

--- src/utils/notifications/push.js
+++ src/utils/notifications/push.js
@@ -2,7 +2,9 @@ import Logger from '@/utils/logger'
 const logger = Logger('notifications:push')
 
 export function requestPermission () {
-  Notification.requestPermission(function (status) {
-    logger.log('Notification permission status:', status)
-  })
+  if (typeof Notification !== 'undefined') {
+    Notification.requestPermission(function (status) {
+      logger.log('Notification permission status:', status)
+    })
+  }
 }
