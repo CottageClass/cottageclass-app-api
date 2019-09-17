1027c24e1d8fd011c54956011f7c5c57f292e574
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-17 17:44:53 -0400

    firebase sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,1 +1,23 @@
-// content for me please
+/* global importScripts firebase */
+
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js')
+
+firebase.initializeApp({
+  'messagingSenderId': '438514874280'
+})
+
+const messaging = firebase.messaging()
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload)
+  // Customize notification here
+  const notificationTitle = payload.data.title
+  const notificationOptions = {
+    body: payload.data.body,
+    icon: '/lilypad-icon-192x192.png',
+    badge: '/lilypad-icon-192x192.png'
+  }
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions)
+})
