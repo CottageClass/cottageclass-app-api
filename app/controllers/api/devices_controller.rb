5b02e93f37cda2a1f12414e49a35fb919f653f5f
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-17 10:05:01 -0400

    route and controller for devices

---
+++ app/controllers/api/devices_controller.rb
@@ -0,0 +1,19 @@
+class API::DevicesController < API::BaseController
+  before_action :authenticate_user!
+
+  def create
+    device = current_user.devices.build safe_params
+    if device.save
+      render status: :created
+    else
+      render json: { errors: device.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def safe_params
+    params.require(:device).permit :token
+  end
+end
