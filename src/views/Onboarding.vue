0fd9c2b0709dbe0a934f161c059d889018b702dc
holmesworcester <hwilson@gmail.com>
2019-09-24 10:56:10 -0400

    Removes "offer a playdate" step from onboarding flow

--- src/views/Onboarding.vue
+++ src/views/Onboarding.vue
@@ -5,17 +5,7 @@
         <StyleWrapper styleIs="onboarding">
           <UserInformation v-if="section==='user-info'"
                            :stepName="stepName"
-                           @finished="finishUserInfo"/>
-          <CreateEvent v-if="section==='offering'"
-                       :stepName="stepName"
-                       context="onboarding"
-                       @finished="collectHomeInfo"
-                       @skip="collectUserDetails"
-          />
-          <HouseInformation v-if="section==='home-info'"
-                            :stepName="stepName"
-                            context="onboarding"
-                            @finished="collectUserDetails" />
+                           @finished="collectUserDetails" />
           <UserDetails v-if="section==='user-details'"
                        :stepName="stepName"
                        @finished="finsishOnboarding" />
@@ -29,37 +19,24 @@
 import StyleWrapper from '@/components/FTE/StyleWrapper'
 import UserInformation from '@/components/FTE/userInformation/UserInformation'
 import UserDetails from '@/components/FTE/userInformation/UserDetails'
-import CreateEvent from '@/components/CreateEvent'
-import HouseInformation from '@/components/FTE/userInformation/HouseInformation'
 
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'Onboarding',
   props: ['stepName', 'section'],
-  components: { StyleWrapper, UserInformation, CreateEvent, HouseInformation, UserDetails },
+  components: { StyleWrapper, UserInformation, UserDetails },
   computed: mapGetters([ 'redirectRoute', 'firstCreatedEvent' ]),
   methods: {
     finsishOnboarding () {
       if (this.redirectRoute) {
         this.$router.push(this.redirectRoute)
       } else {
-        if (this.firstCreatedEvent) {
-          this.$router.push({ name: 'SocialEventInvite', params: { id: this.firstCreatedEvent.id } })
-        } else {
-          // in the case that the event creation was skipped, we go straight to home
-          this.$router.push({ name: 'Search' })
-        }
+        this.$router.push({ name: 'Search' })
       }
     },
     collectUserDetails () {
       this.$router.push({ params: { section: 'user-details', stepName: null } })
-    },
-    collectHomeInfo () {
-      this.$router.push({ params: { section: 'home-info', stepName: null } })
-    },
-    finishUserInfo (e) {
-      this.$router.push({ params: { section: 'offering', stepName: null } })
     }
   },
   created () {
