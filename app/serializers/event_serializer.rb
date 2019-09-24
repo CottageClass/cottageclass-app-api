6a6de842fb7d8bcb2a4add38f73ee46bb9d16371
Ari Kardasis <ari.kardasis@gmail.com>
2019-09-24 16:02:11 -0400

    add place to event response

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -3,7 +3,7 @@ class EventSerializer
 
   has_many :participants
 
-  attributes(*(%i[name maximum_children child_age_minimum child_age_maximum] + 
+  attributes(*(%i[name maximum_children child_age_minimum child_age_maximum] +
                User::PUBLIC_ATTRIBUTES.map { |attribute| format('host_%s', attribute).to_sym }))
   attribute(:starts_at) { |instance| (instance.in_instance_time_zone instance.starts_at).to_s :iso8601 }
   attribute(:ends_at) { |instance| (instance.in_instance_time_zone instance.ends_at).to_s :iso8601 }
@@ -12,7 +12,14 @@ class EventSerializer
   attribute :participated, if: proc { |_, params| params.dig(:current_user).present? } do |instance, params|
     instance.participated? params[:current_user]
   end
-
+  attribute :place do |instance, _params|
+    if instance.place.nil?
+      nil
+    else
+      serializer = PlaceSerializer.new instance.place
+      serializer.serializable_hash
+    end
+  end
   attribute :host do |instance, params|
     serializer = PublicUserSerializer.new instance.user,
                                           include: %i[children],
