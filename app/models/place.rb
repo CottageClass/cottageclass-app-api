588e0efbcc6e0c7b2f6fd74540a3f2ad5e32b437
Ari Kardasis <ari.kardasis@gmail.com>
2019-10-04 13:42:37 -0400

    fetch additional place information

--- app/models/place.rb
+++ app/models/place.rb
@@ -34,6 +34,11 @@ class Place < ApplicationRecord
     assign_attributes location
   end
 
+  def update_details_from_api
+    retrieve_details
+    save
+  end
+
   private
 
   def generate_time_zone
@@ -46,12 +51,16 @@ class Place < ApplicationRecord
     @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
     if google_id.present? && @latitude.blank?
       req = @client.spot(google_id)
+      self.full_result_object = req.json_result_object
       self.latitude = req.lat
       self.longitude = req.lng
       self.full_address = req.formatted_address
       self.name = req.name
       self.locality = req.city
       self.admin_area_level_1 = req.region
+      self.vicinity = req.vicinity
+      neighborhood_component = req.address_components.find { |component| component['types'].include? 'neighborhood' }
+      self.neighborhood = neighborhood_component['long_name'] if neighborhood_component.present?
     end
   end
 end
