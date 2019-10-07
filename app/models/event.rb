dddbe0c9cf7137dfc6a40c7d7eba71a9cbe20f00
Ari Kardasis <ari.kardasis@gmail.com>
2019-10-07 11:33:00 -0400

    digest emailsdaily

--- app/models/event.rb
+++ app/models/event.rb
@@ -30,6 +30,10 @@ class Event < ApplicationRecord
   scope :nearest, ->(user) { where.not(id: user.events).near([user.place.latitude, user.place.longitude], 100) }
   scope :past, -> { where(Event.arel_table[:starts_at].lt(Time.current)).order starts_at: :desc }
   scope :upcoming, -> { where(Event.arel_table[:starts_at].gteq(1.hour.ago(Time.current))).order starts_at: :asc }
+  scope :next_day, lambda {
+                     where(Event.arel_table[:starts_at].gteq(1.hour.ago(Time.current)))
+                       .where(Event.arel_table[:starts_at].lteq(1.day.since(Time.current)))
+                   }
   scope :notifiable, lambda {
     where(
       Event.arel_table[:ends_at].between(1.hour.ago(Time.current)..1.hour.since(Time.current))
@@ -68,8 +72,16 @@ class Event < ApplicationRecord
     in_instance_time_zone(starts_at).strftime('%B %e, %Y').try :squish
   end
 
+  def start_date_abbreviated
+    in_instance_time_zone(starts_at).strftime('%-m/%-e').try :squish
+  end
+
   def start_day
     in_instance_time_zone(starts_at).strftime('%A').try :squish
+  end
+
+  def start_day_abbreviated
+    in_instance_time_zone(starts_at).strftime('%a').try :squish
   end
 
   def time_range
@@ -78,7 +90,7 @@ class Event < ApplicationRecord
   end
 
   def end_time
-    in_instance_time_zone(ends_at).strftime('%l:%M')
+    in_instance_time_zone(ends_at).strftime('%l:%M %P')
   end
 
   def start_time
