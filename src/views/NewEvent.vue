d5c489c8de1ac2973e4980a298ef2320b03ba78f
AsaNeedle <asa@cottageclass.com>
2019-10-08 13:36:53 -0400

    event name change

--- src/views/NewEvent.vue
+++ src/views/NewEvent.vue
@@ -4,7 +4,7 @@
       <StyleWrapper styleIs="onboarding">
         <CreateEvent v-if="section==='event'"
                      :stepName="stepName"
-                     v-on:seriesOfEvents="findUpcomingEvent"
+                     v-on:submission-complete="findUpcomingEvent"
                      @finishedHomeEvent="completeCreationForHomeEvents"
                      @finishedPublicEvent="proceed"
                      context="new-event"
