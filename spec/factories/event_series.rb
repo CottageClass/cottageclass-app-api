7757592c81d8e9592c761c4dda63df0828505111
Ari Kardasis <ari.kardasis@gmail.com>
2019-10-11 12:05:08 -0400

    add phone numbers for notification tests

--- spec/factories/event_series.rb
+++ spec/factories/event_series.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :event_series do
-    user { build :user, :with_place }
+    user { build :user, :with_place, :with_phone_number }
     name { Faker::Lorem.unique.sentence }
     start_date { Faker::Date.unique.between 2.weeks.ago, 1.week.ago }
     starts_at { Faker::Time.unique.between 2.weeks.ago, 1.week.ago }
