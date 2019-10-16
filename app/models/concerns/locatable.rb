427dd592679d1852900225272e27991abd809160
Ari Kardasis <ari.kardasis@gmail.com>
2019-10-16 15:48:39 -0400

    add locatable concern

---
+++ app/models/concerns/locatable.rb
@@ -0,0 +1,18 @@
+require 'active_support/concern'
+
+module Locatable
+  extend ActiveSupport::Concern
+
+  included do
+    before_save :update_location, if: lambda { |instance|
+      instance.latitude.blank? || instance.longitude.blank? ||
+        (instance.respond_to?(:place_changed) && instance.place_changed?) ||
+        (instance.respond_to?(:place_id_changed) && instance.place_id_changed?)
+    }
+  end
+
+  def update_location
+    self.latitude = place.latitude if place.present?
+    self.longitude = place.longitude if place.present?
+  end
+end
