fc626e1f95dee1ff399b6ac9c105bfdd0de7c999
Ari Kardasis <ari.kardasis@gmail.com>
2019-10-16 15:54:26 -0400

    add geocoding to Locatables

--- app/models/concerns/locatable.rb
+++ app/models/concerns/locatable.rb
@@ -4,6 +4,7 @@ module Locatable
   extend ActiveSupport::Concern
 
   included do
+    reverse_geocoded_by :latitude, :longitude
     before_save :update_location, if: lambda { |instance|
       instance.latitude.blank? || instance.longitude.blank? ||
         (instance.respond_to?(:place_changed) && instance.place_changed?) ||
