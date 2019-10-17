3ea2e2f690b64638ccf99f9d66efda02683807b0
AsaNeedle <asa@cottageclass.com>
2019-10-17 16:55:49 -0400

    lint

--- src/components/base/AvatarImage.vue
+++ src/components/base/AvatarImage.vue
@@ -21,11 +21,11 @@ export default {
       return parseInt(this.imageSize)
     },
     url: function () {
-      if (this.event){
-        if (this.event.images.length > 0){
+      if (this.event) {
+        if (this.event.images.length > 0) {
           return avatarUrl(this.event.images[0], this.physicalSize)
-          }
-        } 
+        }
+      }
       if (this.person) {
         if (this.person.avatar) {
           return avatarUrl(this.person.avatar, this.physicalSize)
