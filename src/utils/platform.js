d365f8cfe9031afe2687ece1db712169f7f90d41
Ari Kardasis <ari.kardasis@gmail.com>
2019-10-22 13:02:23 -0400

    add test for iphoneX

--- src/utils/platform.js
+++ src/utils/platform.js
@@ -9,3 +9,14 @@ export function isPWA () {
 export function isNative () {
   return isIOSNativeApp() || isPWA()
 }
+
+export function isIPhoneX () {
+  // somewhat hacky but effective way to detect if it's an iPhone X (and therefore if it has a notch)
+  var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+  var ratio = window.devicePixelRatio || 1
+  var screen = {
+    width: window.screen.width * ratio,
+    height: window.screen.height * ratio
+  }
+  return (iOS && screen.width === 1125 && screen.height === 2436)
+}
