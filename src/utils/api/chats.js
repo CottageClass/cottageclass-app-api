85c569ecf7588fd7d66203eea1f41a8fbe26459d
Ari Kardasis <ari.kardasis@gmail.com>
2019-10-29 14:19:33 -0400

    utils and api

--- src/utils/api/chats.js
+++ src/utils/api/chats.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import normalize from 'json-api-normalizer'
 
 import { createConversations } from '../createConversation'
+import { createMessages } from '../createMessage'
 import Logger from '@/utils/logger'
 const logger = Logger('api:events')
 
@@ -10,6 +11,20 @@ export const fetchConversations = async () => {
     const res = await axios.get(`/api/user/conversations`)
     if (res) {
       return createConversations(normalize(res.data))
+    } else {
+      throw Error('failed to fetch conversations')
+    }
+  } catch (e) {
+    logger.logError(e)
+    throw e
+  }
+}
+
+export const fetchMessages = async (userId) => {
+  try {
+    const res = await axios.get(`/api/users/${userId}/messages`)
+    if (res) {
+      return createMessages(normalize(res.data))
     } else {
       throw Error('failed to fetch conversations')
     }
