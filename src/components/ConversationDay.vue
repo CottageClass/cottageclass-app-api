e1a9782915253d9795c76385d91cb30926e692fb
AsaNeedle <asa@cottageclass.com>
2019-11-06 11:45:41 -0500

    removed extra css

--- src/components/ConversationDay.vue
+++ src/components/ConversationDay.vue
@@ -50,43 +50,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.chat-detail--day {
-  position: relative;
-  display: flex;
-  padding-bottom: 12px;
-  flex-direction: column;
-  align-items: center;
-  background-color: #fff;
-}
-
-.chat-detail--day-text {
-  position: static;
-  top: 108px;
-  width: 100%;
-  background-color: transparent;
-  color: #b3b3b3;
-  text-align: center;
-}
-
-.divider {
-  position: absolute;
-  left: 0%;
-  top: 10px;
-  right: 0%;
-  bottom: auto;
-  z-index: 0;
-  width: 100%;
-  height: 1px;
-  min-height: 1px;
-  background-color: rgba(0, 0, 0, 0.06);
-}
-
-.text-white-background {
-  position: relative;
-  z-index: 0;
-  padding-right: 8px;
-  padding-left: 8px;
-  flex: 0 auto;
-  background-color: #fff;
-}
 </style>
