3d54ac112859c75f9007008e6673bd940ef6c642
Ari Kardasis <ari.kardasis@gmail.com>
2019-11-06 08:50:23 -0500

    unclean fetch method for places

---
+++ src/utils/api/places.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import normalize from 'json-api-normalizer'
+
+import Logger from '@/utils/logger'
+const logger = Logger('api:events')
+
+export const fetchPlace = async (id) => {
+  try {
+    const res = await axios.get(`/api/place/${id}`)
+    if (res) {
+      return normalize(res.data)
+    } else {
+      throw Error('failed to fetch place')
+    }
+  } catch (e) {
+    logger.logError(e)
+    throw e
+  }
+}
+
+export const updatePlace = async (id, data) => {
+  try {
+    const res = await axios.post(
+      `/api/places/${id}`,
+      data
+    )
+    if (res) {
+      return normalize(res.data)
+    } else {
+      throw Error('failed to submit message')
+    }
+  } catch (e) {
+    logger.logError(e)
+    throw e
+  }
+}
