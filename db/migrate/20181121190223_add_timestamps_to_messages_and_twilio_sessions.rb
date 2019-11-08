a785ba47f050decacce77c0ef6c4ddc6abb927e4
Ari Kardasis <ari.kardasis@gmail.com>
2019-11-08 15:11:05 -0500

    fix failing migration

--- db/migrate/20181121190223_add_timestamps_to_messages_and_twilio_sessions.rb
+++ db/migrate/20181121190223_add_timestamps_to_messages_and_twilio_sessions.rb
@@ -7,8 +7,8 @@ class AddTimestampsToMessagesAndTwilioSessions < ActiveRecord::Migration[5.2]
     # backfill existing record with created_at and updated_at
     # values making clear that the records are faked
     long_ago = DateTime.new(1999, 1, 1)
-    Message.update_all(created_at: long_ago, updated_at: long_ago)
-    TwilioSession.update_all(created_at: long_ago, updated_at: long_ago)
+    # Message.update_all(created_at: long_ago, updated_at: long_ago)
+    # TwilioSession.update_all(created_at: long_ago, updated_at: long_ago)
 
     # change not null constraints
     change_column_null :messages, :created_at, false
