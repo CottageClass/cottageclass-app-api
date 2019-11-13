ac13d97c1776357ae037c5545454324d1d161d23
AsaNeedle <asa@cottageclass.com>
2019-11-13 11:25:00 -0500

    schema.rb as a result of migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_11_06_012736) do
-
+ActiveRecord::Schema.define(version: 2019_11_13_133349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
