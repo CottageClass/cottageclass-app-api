abcd31597addba623664ae93cc49508c3bf87572
Ari Kardasis <ari.kardasis@gmail.com>
2019-11-19 16:57:53 -0500

    remove 'full' from event

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -7,7 +7,6 @@ class EventSerializer
   attribute(:starts_at) { |instance| (instance.in_instance_time_zone instance.starts_at).to_s :iso8601 }
   attribute(:ends_at) { |instance| (instance.in_instance_time_zone instance.ends_at).to_s :iso8601 }
   attribute(:participants_count) { |instance| instance.participants.count }
-  attribute :full, &:full?
   attribute :participated, if: proc { |_, params| params.dig(:current_user).present? } do |instance, params|
     instance.participated? params[:current_user]
   end
