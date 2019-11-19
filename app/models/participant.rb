02c566997eba57957e1788cdd052155b187d910d
Ari Kardasis <ari.kardasis@gmail.com>
2019-11-19 17:06:48 -0500

    remove bogus validation

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,10 +1,5 @@
 class Participant < ApplicationRecord
   validates_associated :participant_children
-  validate do |instance|
-    if instance.participable.present?
-      instance.errors.add(:base, :invalid, message: 'exceeds maximum number of children')
-    end
-  end
 
   belongs_to :participable, polymorphic: true, inverse_of: :participants
   belongs_to :user, inverse_of: :participants
