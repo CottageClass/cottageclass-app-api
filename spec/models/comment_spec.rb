3748cbdbd017e965db0ef637f8c57d0609d7d540
Ari Kardasis <ari.kardasis@gmail.com>
2019-12-02 09:43:54 -0500

    test, factory and implementation of recent scope

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to belong_to(:sender).inverse_of(:comments) }
     it { is_expected.to belong_to(:group).inverse_of(:comments) }
   end
+
+  context 'scope' do
+    describe 'recent' do
+      let(:new_comment) { FactoryBot.create :comment, created_at: 47.hours.ago }
+      let(:old_comment) { FactoryBot.create :comment, created_at: 49.hours.ago }
+
+      it 'includes correct comments' do
+        comments = described_class.recent
+        expect(comments).to include new_comment
+        expect(comments).not_to include old_comment
+      end
+    end
+  end
 end
