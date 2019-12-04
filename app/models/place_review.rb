6193eb51ca65999d08b47b5e03225cecaea84a4c
Ari Kardasis <ari.kardasis@gmail.com>
2019-12-04 13:08:30 -0500

    stars and content validations and tests

--- app/models/place_review.rb
+++ app/models/place_review.rb
@@ -1,4 +1,11 @@
 class PlaceReview < ApplicationRecord
   belongs_to :place, inverse_of: :place_reviews
   belongs_to :user, inverse_of: :place_reviews
+
+  validates :stars,
+            presence: true,
+            numericality: { only_integer: true,
+                            greater_than_or_equal_to: 1,
+                            less_than_or_equal_to: 5 }
+  validates :content, presence: true
 end
